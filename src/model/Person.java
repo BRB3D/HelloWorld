921c54f227d815376674d84e28fb71c2b336ec56
BRB3D <samuraix221@hotmail.com>
2022-04-01 15:04:31 -0400

    Create Class, getter, setter for person and address

---
+++ src/model/Person.java
@@ -0,0 +1,40 @@
+package model;
+
+import java.time.LocalDate;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private LocalDate dob;
+    private Address address;
+
+    public Person(String firstName, String lastName, LocalDate dob) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dob = dob;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+}
